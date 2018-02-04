f1d9377b9574a1c850ccaa09ddbb93306a6bc4bf
mao <maodipu@dingtalk.com>
2018-02-04 23:44:44 +0800

    数据表审计功能mybatis拦截器设计

---
+++ common/common-model-mybatis/src/main/java/com/common/model/po/settlement/Balance.java
@@ -0,0 +1,9 @@
+package com.common.model.po.settlement;
+
+import com.common.model.CrudEntity;
+import com.common.mybatis.annotation.AuditEnable;
+
+@AuditEnable(table = "balance_a")
+public class Balance extends CrudEntity {
+
+}
